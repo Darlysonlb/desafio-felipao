f23ef41e0d8780a7d14325e881fae5f0e4d8fb48
Darlysonlb <darlysonlb@gmail.com>
2023-10-28 14:26:23 -0300

    Desafio 01 atualizado, desafio 02 concluido usando o mesmo padrão do primeiro

---
+++ Desafio 02/index.js
@@ -0,0 +1,42 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function saldoVitoria (vitoria, derrota){
+    let saldo = vitoria - derrota
+    return saldo
+}
+
+//Solicita ao usuario o numero de Vitorias e Derrotas
+rl.question('Número de Vitórias: ', (vitoria) => {
+    rl.question('Número de Derrotas: ', (derrota) => {
+                        
+        let vitoriasPositivas = saldoVitoria(parseInt(vitoria), parseInt(derrota));
+        let nivel;
+
+        if ( vitoriasPositivas < 10) {
+            nivel = 'Ferro';
+        } else if (vitoriasPositivas >= 11 && vitoriasPositivas <= 20) {
+            nivel = 'Bronze';
+        } else if (vitoriasPositivas >= 21 && vitoriasPositivas <= 50) {
+            nivel = 'Prata';
+        } else if (vitoriasPositivas >= 51 && vitoriasPositivas <= 80) {
+            nivel = 'Ouro';
+        } else if (vitoriasPositivas >= 81 && vitoriasPositivas <= 90) {
+            nivel = 'Diamante';
+        } else if (vitoriasPositivas >= 91 && vitoriasPositivas <= 100) {
+            nivel = 'Lendário';
+        } else {
+            nivel = 'Imortal';
+        }
+
+        // Exibe a mensagem com o nome e o nível do herói
+        console.log(`O Herói tem de saldo de ${vitoriasPositivas} está no nível de ${nivel}`);
+
+       rl.close();
+    })
+});
